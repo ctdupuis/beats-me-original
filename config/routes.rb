753356c23af383131f575d66042924d495aa5476
ctdupuis <ctdupuis7@gmail.com>
2020-03-03 23:05:04 -0600

    working associations on new song form, still fix new album form

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
   resources :users, only: [:create, :show]
+  resources :artists
   resources :songs
   resources :albums do 
     resources :songs 
+    resources :artists, only: [:new, :create, :show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
