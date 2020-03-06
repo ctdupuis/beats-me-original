e89d39f08ae9bbe3c86602bb3ba9ab87f8b394cd
ctdupuis <ctdupuis7@gmail.com>
2020-03-05 23:27:30 -0600

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 genres = ["Hip Hop",
         "Jazz",   
         "Rock",
@@ -56,3 +49,65 @@ genres.each do |genre|
     Genre.create(name: genre)
 end
 
+def make_seeds(track_hash, album, artist, genre)
+    genre = Genre.find_or_create_by(name: genre)
+    artist = Artist.find_or_create_by(name: artist)
+    album = Album.create(name: album, artist_name: artist.name, genre_id: genre.id)
+    track_hash.each do |k, v|
+        song = Song.new(v)
+        song.artist_id = artist.id 
+        song.save
+        album.songs << song
+    end
+end
+
+up_tracks = {
+    :t1 => {:title => "Intro", :length => "3:02"},
+    :t2 => {:title => "Soul Food", :length => "4:52"},
+    :t3 => {:title => "I'm Gone", :length => "4:42"},
+    :t4 => {:title => "Gang Related", :length => "2:47"},
+    :t5 => {:title => "Buried Alive", :length => "5:37"},
+    :t6 => {:title => "Bounce", :length => "4:04"}
+}
+    
+swimming_tracks = {
+    :t1 => {:title => "Come Back to Earth", :length => "2:41"},
+    :t2 => {:title => "Hurt Feelings", :length => "4:05"},
+    :t3 => {:title => "What's the Use?", :length => "4:48"},
+    :t4 => {:title => "Perfecto", :length => "3:35"},
+    :t5 => {:title => "Self Care", :length => "5:45"},
+    :t6 => {:title => "Wings", :length => "4:10"}
+}
+
+circles_tracks = {
+    :t1 => {:title => "Circles", :length => "2:50"},
+    :t2 => {:title => "Complicated", :length => "3:52"},
+    :t3 => {:title => "Blue World", :length => "3:29"},
+    :t4 => {:title => "Good News", :length => "5:42"},
+    :t5 => {:title => "I Can See", :length => "3:40"},
+    :t6 => {:title => "Everybody", :length => "4:16"}
+}
+
+fhd_tracks = {
+    :t1 => {:title => "Intro", :length => "2:09"},
+    :t2 => {:title => "January 28th", :length => "4:02"},
+    :t3 => {:title => "Wet Dreamz", :length => "3:59"},
+    :t4 => {:title => "03' Adolescence", :length => "4:24"},
+    :t5 => {:title => "A Tale of 2 Citiez", :length => "4:29"},
+    :t6 => {:title => "Fire Squad", :length => "4:48"}
+}
+
+hollywood_tracks = {
+    :t1 => {:title => "Hollywood's Bleeding", :length => "2:36"},
+    :t2 => {:title => "Saint-Tropez", :length => "2:30"},
+    :t3 => {:title => "Enemies (feat. Da Baby)", :length => "3:16"},
+    :t4 => {:title => "Allergic", :length => "2:36"},
+    :t5 => {:title => "A Thousand Bad Times", :length => "3:41"},
+    :t6 => {:title => "Circles", :length => "3:35"}
+}
+
+make_seeds(up_tracks, "Under Pressure", "Logic", "Rap")
+make_seeds(circles_tracks, "Circles", "Mac Miller", "Hip Hop")
+make_seeds(swimming_tracks, "Swimming", "Mac Miller", "Hip Hop")
+make_seeds(fhd_tracks, "2014 Forest Hills Drive", "J. Cole", "Rap")
+make_seeds(hollywood_tracks, "Hollywood's Bleeding", "Post Malone", "Pop")
